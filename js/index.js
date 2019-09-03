2bab9033741f8296fea960d07ba55c92b1d9255c
Chrismis79 <chrismis79@gmail.com>
2019-09-03 17:43:35 -0500

    6events done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,37 @@
 // Your code goes here
 
+window.addEventListener('resize', (e) =>{
+    const body = document.querySelector('body');
+    body.style.background = "yellow";
+})
+
+const heading = document.querySelector('.logo-heading');
+heading.addEventListener('mouseover', () => {
+    heading.style.color='red';    
+})
+heading.addEventListener('mouseleave',()=>{
+    heading.style.color='black';
+})
+
+const navLinks = document.querySelectorAll('.nav-link');
+console.log(navLinks);
+navLinks.forEach(item => {
+    item.addEventListener('mouseover', e =>{
+        item.style.color="red";
+    })
+})
+navLinks.forEach(item => {
+    item.addEventListener('mouseleave', e =>{
+        item.style.color="black";
+    })
+})
+const welcomeAni = document.querySelector('h2');
+welcomeAni.addEventListener('dblclick', (e) =>{
+e.target.animate([
+    {transform: 'translateY(0px)'},
+    {transform: 'translateY(-300px)'}
+], {
+    duration: 1000,
+    iterations: 3
+})
+});
