64961e859aac0fd731e488114abd0dd4c6cda9b8
Chrismis79 <chrismis79@gmail.com>
2019-09-03 19:46:44 -0500

    MVP COMPLETE

--- js/index.js
+++ js/index.js
@@ -35,3 +35,57 @@ e.target.animate([
     iterations: 3
 })
 });
+
+const mainNav = document.querySelector('.main-navigation');
+mainNav.classList.add('scroll');
+mainNav.classList.add('myP');
+mainNav.style.position = "fixed";
+const scrolling = document.querySelector('.scroll');
+scrolling.style.background = "yellow";
+console.log(mainNav);
+
+window.scroll = function() {myFunction()};
+
+function myFunction(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop> 50){
+        document.getElementByclassName('myP').className = "scroll";
+    }else {
+        document.getElementclassName('myP').className = "";
+    }
+}
+
+const headerImgAni = document.querySelector('img');
+headerImgAni.addEventListener('mouseover', (e) =>{
+console.log('mouse over image');
+    e.target.animate([
+    {transform: 'translateX(0px)'},
+    {transform: 'translateX(300px)'}
+], {
+    duration: 1000,
+    iterations: 3
+})
+});
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(element =>{
+    element.addEventListener('click', (e) => {
+        element.style.backgroundColor = "orange";
+        e.stopPropagation();
+ })
+})
+
+const destinationDiv = document.querySelectorAll('.destination');
+
+destinationDiv.forEach(element =>{
+    element.addEventListener('click', (e) =>{
+        element.style.backgroundColor = "red";
+        
+    });
+})
+
+const homeLink = document.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+
